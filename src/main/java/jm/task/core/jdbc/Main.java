f99dc3b0f4f33bf6afe5651db2627ae0246ee8fb
Max-None <106478561+Max-None@users.noreply.github.com>
2022-08-04 15:47:20 +0300

    PP 1.1.3 corrections after mentor review

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,12 +1,13 @@
 package jm.task.core.jdbc;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        UserServiceImpl userService = new UserServiceImpl();
+        UserService userService = new UserServiceImpl();
         userService.createUsersTable();
         userService.saveUser("Tony", "Tester", (byte) 25);
         userService.saveUser("Harry", "Hacker", (byte) 35);
