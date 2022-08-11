b7ece315dd774d1c86e5ea7060bbacab02bc8a50
Max-None <106478561+Max-None@users.noreply.github.com>
2022-08-11 13:20:22 +0300

    PP 1.1.4

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,7 +1,8 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
-
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.Session;
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
@@ -12,31 +13,53 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void createUsersTable() {
+        new UserDaoJDBCImpl().createUsersTable();
 
     }
 
     @Override
     public void dropUsersTable() {
-
+        new UserDaoJDBCImpl().dropUsersTable();
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-
+        try (Session session = Util.getSession()) {
+            session.beginTransaction();
+            User user = new User(name, lastName, age);
+            session.save(user);
+            System.out.println("User с именем – " + name + " добавлен в базу данных");
+            session.getTransaction().commit();
+        }
     }
 
     @Override
     public void removeUserById(long id) {
-
+        try (Session session = Util.getSession()) {
+            session.beginTransaction();
+            User user = session.get(User.class, id);
+            session.remove(user);
+            session.getTransaction().commit();
+        }
     }
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+        List<User> userList;
+        try (Session session = Util.getSession()) {
+            session.beginTransaction();
+            userList = session.createQuery("SELECT u FROM User u", User.class).getResultList();
+            session.getTransaction().commit();
+        }
+        return userList;
     }
 
     @Override
     public void cleanUsersTable() {
-
+        try (Session session = Util.getSession()) {
+            session.beginTransaction();
+            session.createQuery("DELETE FROM User").executeUpdate();
+            session.getTransaction().commit();
+        }
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,9 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.Session;
+import org.hibernate.cfg.Configuration;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -14,6 +18,17 @@ public class Util {
             e.printStackTrace();
         }
         return connection;
+    }
+
+    public static Session getSession() {
+        return new Configuration().addAnnotatedClass(User.class)
+                .setProperty("hibernate.driver_class", "com.mysql.cj.jdbc.Driver")
+                .setProperty("hibernate.connection.url", "jdbc:mysql://localhost:3306/myusers")
+                .setProperty("hibernate.connection.username", "root")
+                .setProperty("hibernate.connection.password", "MySqlElinel1")
+                .setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL8Dialect")
+                .setProperty("hibernate.current_session_context_class", "thread")
+                .buildSessionFactory().getCurrentSession();
     }
 
 }
